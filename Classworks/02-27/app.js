2dfa41dcb5c4e5dd6b5562d250e435e6e7db8656
karolis <buta.karolis@gmail.com>
2020-02-27 21:41:52 +0200

    Javascript

--- Classworks/02-27/app.js
+++ Classworks/02-27/app.js
@@ -1,58 +1,64 @@
-var x = 5 + 5;
-var y = "5" + 5;
-var z = "Hello" + 5;
-document.getElementById("demo").innerHTML =
-    x + "<br>" + y + "<br>" + z;
+// var x = 5 + 5;
+// var y = "5" + 5;
+// var z = "Hello" + 5;
+// document.getElementById("demo").innerHTML =
+//     x + "<br>" + y + "<br>" + z;
+//
+// let arr = ( 'hello 1 world');
+// console.log(arr)
+// var d = 3;
+// var e =  4;
+// var z = d * e;
+// console.log(d,e,z)
+//
+//
+// var x = 1 ;
+// var e = 2 ;
+// var w = 3;
+// var q = 7;
+// const p = (x + e ) * (q * w);
+// console.log(p);
+//
+// let year = 2020;
+//
+// console.log(year);
+//
+// const xs = 50;
+// const rs = 2;
+// const l = xs * rs ;
+// document.getElementById("demo").innerHTML =
+//     xs + "<br>" + rs + "<br>" + l;
+// console.log(l);
+//
+// var a = 2;
+// var c = 3;
+// var b = a + c;
+// console.log(b);
+//
+// let name = "tomas";
+// let birthday = "geguzes 12";
+// console.log (name + ' yra gimes '+ birthday);
+//
+//
+//
+// var atsakymas = prompt("Please enter your name", "Harry Potter");
+// console.log(atsakymas);
+//
+// var klausimas = prompt("bazinis uzmokestis iveskite");
+// var z = 1761463 * klausimas ;
+// var atsakymas = (z * 100 ) /  8487300000 ;
+// document.write("atsakymas");
+// console.log(klausimas);
+// console.log(z)
+// console.log(atsakymas + "%")
+//
+// let baseamount = prompt('Įveskite bazinį išmokos dydį:');
+// let workingpeople = 1761463;
+// let budget = 8487300000;
+// document.write(`Bazinė išmoka sudarys ${baseamount * workingpeople*100/budget} % metinio Lietuvos biudžeto`);
 
-let arr = ( 'hello 1 world');
-console.log(arr)
-var d = 3;
-var e =  4;
-var z = d * e;
-console.log(d,e,z)
+var username = vardas
+var password = master
+prompt(suveskite prisijungimo varda ir slaptazodi )
 
 
-var x = 1 ;
-var e = 2 ;
-var w = 3;
-var q = 7;
-const p = (x + e ) * (q * w);
-console.log(p);
-
-let year = 2020;
-
-console.log(year);
-
-const xs = 50;
-const rs = 2;
-const l = xs * rs ;
-document.getElementById("demo").innerHTML =
-    xs + "<br>" + rs + "<br>" + l;
-console.log(l);
-
-var a = 2;
-var c = 3;
-var b = a + c;
-console.log(b);
-
-let name = "tomas";
-let birthday = "geguzes 12";
-console.log (name + ' yra gimes '+ birthday);
-
-
-
-var atsakymas = prompt("Please enter your name", "Harry Potter");
-console.log(atsakymas);
-
-var klausimas = prompt("bazinis uzmokestis iveskite");
-var z = 1761463 * klausimas ;
-var atsakymas = (z * 100 ) /  8487300000 ;
-document.write("atsakymas");
-console.log(klausimas);
-console.log(z)
-console.log(atsakymas + "%")
-
-let baseamount = prompt('Įveskite bazinį išmokos dydį:');
-let workingpeople = 1761463;
-let budget = 8487300000;
-document.write(`Bazinė išmoka sudarys ${baseamount * workingpeople*100/budget} % metinio Lietuvos biudžeto`);
