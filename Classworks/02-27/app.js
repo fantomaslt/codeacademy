651594decf88f95671813e0291ccaec02e2128eb
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-05 16:59:23 +0200

    naujas failas

--- Classworks/02-27/app.js
+++ Classworks/02-27/app.js
@@ -180,7 +180,167 @@ alert("jusu sutapimas yra " + loveScore + " % ");
 // alert(" Your name " + username);
 
 
+// love score patobulinta su IF ir Else
+// prompt("iveskite savo ir kito varda") ;
+// prompt("ivesk antra varda");
+//
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1 ; //1-100 gaunam skaiciu
+//
+// if (loveScore > 70) {
+//     alert("jusu sutapimas yra "  + loveScore + "%" + "ahubybienai");
+// }
+// if(loveScore > 30 && loveScore <= 70){
+//     alert("jusu sutapimas yra "  + loveScore + "%");
+// }
+// else
+// {
+//     alert("jusu sutapimas yra "  + loveScore + "%" + "Niek gero is Jusu ner ");
+// }
+// console.log(loveScore);
+
+// function bmiCalculator (weight, height) {
+//     return interpretation;
+// }
+//
+// if (bmi<18.5) {
+//     (interpretation = "Your BMI," + BMI + ", so you are underweight."
+// }
+// if (18.5<=bmi=>24.9) {
+//     (interpretation = "Your BMI," + BMI + ", so you have normal weight."
+// }
+// if (bmi>24.9) {
+//     (interpretation = "Your BMI," + BMI + ", so you are overweight."
+// }
+
+/* --------------  Keliamieji metai ------------- */
+//
+// function isLeap(year) {
+//     if (year % 4 === 0) {
+//         if (year % 100 === 0) {
+//             if (year % 400 === 0) {
+//                 return "Leap year";
+//             } else {
+//                 return "not leaps year";
+//             }
+//         else
+//             {
+//                 return "Leap year";
+//             }
+//         }
+//     } else {
+//         return "not leaps year";
+//
+//     }
+// }
+
+// let guests=["pirmas", "antras", "trecias", "ketvirtas","penktas", "sestas", "septintas"];
 
 
 
+
+// Pirma uzduotis
+var array=['a','b','c'];
+document.write(array);
+console.log(array);
+// Antra uzduotis
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+//Trecia uzduotis
+var array2=['a','b','c','d'];
+console.log(array2[0], "+", array2[1],","," ", array2[2], "+",array2[3]);
+// ketvirta uzduotis
+var array3=["2","5","3","9"];
+var rez=(array3[0]*array3[1]+array3[2]*array3[3]);
+console.log(rez);
+//penkta uzduotis
+var obj={a:"1", b:"2", c:"3"};
+console.log(obj["c"]);
+console.log(obj.c)
+// sesta uzduotis
+var week={
+    1:"pirmadienis",
+    2:"antradienis",
+    3:"treciadienis",
+    4:"ketvirtadienis",
+    5:"penktadienis",
+    6:"sestadienis",
+    7:"sekmadienis"
+};
+
+//septinta uzduotis
+
+
+
+//astunta uzduotis
+ var ar=[[1, 2, 3], [4, 5, 6], [7,8,9]];
+ console.log(ar[1][0]);
+
+ //devinta uzduotis
+var masyvas={js: ['jQuery', 'Angular'], php: 'hello', css: 'world'};
+console.log(masyvas.js[0]);
+
+//10 uzduotis
+var dvimatis={ lt:["pirmadienis", "antradienis","treciadienis", "ketvirtadienis", "penktadienis", "sestadienis", "sekmadienis"],
+    en:["monday", "thuesday", "wensday", "thursday", "friday", "saturday","sunday"]};
+console.log(dvimatis.lt[0], " ", dvimatis.en[2]);
+
+// 11 uzduotis
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//     Duotas masyvas 3, 4, 1, 2, 7. Surūšiuokite jį.
+//     Duotas masyvas 1, 2, 3, 4, 5. Konvertuokite masyvą į 1, 2, 3.
+// Duotas masyvas [1, 2, 3, 4, 5]. Konvertuokite masyvą į [1, 4, 5].
+//     For ir While
+// Atspausdinkite skaičių stulpelį nuo 1 iki 100.
+// Atspausdinkite skaičių stulpelį nuo 11 iki 33.
+// Atspausdinkite stulpelį su lyginiais skaičiais nuo 0 iki 100.
+// Naudodami ciklą parodykite sumą nuo 1 iki 100.
+// For ir masyvai
+// Duotas masyvas su elementais [1, 2, 3, 4, 5]. Parodykite visus masyvo elementus ekrane.
+//     Duotas masyvas su elementais [1, 2, 3, 4, 5]. Parodykite šio masyvo elementų sumą.
+//     for-in
+// Duotas objektas green: žalia, red: raudona, blue: mėlyna. Parodykite šio objekto raktus ir elementus.
+//     Duotas objektas su raktais Mantas, Paulius, Mindaugas su reikšmėm 200, 300, 300. Parodykite eilutes tokiu formatu: Mantas - 200 EU atlyginimas.
+//     Duotas masyvas su elementais 2, 5, 9, 15, 0, 4. Naudodami for ir if parodykite masyvo elementus kurie yra daugiau nei 3, bet mažiau nei 10
+// Duotas masyvas su skaičiais. Skaičiai gali būti teigiami ir neigiami. Raskite teigiamų masyvo skaičių sumą.
+//     Duotas masyvas su elementais [1, 2, 3, 4, 5]. Parodykite visus šio masyvo elementus pasinaudoję ciklais for, while
+//     Duotas masyvas su elementais [2, 3, 4, 5]. Parodykite šio masyvo elementų produktą (daugyba), naudokite for ciklą.
+//     Duotas objektas su raktais Vilnius, Riga, Talinas ir reikšmėm Lietuva, Latvija, Estija. Parodykite eilutes tokiu formatu: Vilnius yra Lietuva naudodami for-in ciklą;
+
+
+// //pirma uzduotis
+
+var arr1=[1,2,3];
+var arr2=[4,5,6];
+var arr3 = arr1.concat(arr2);
+console.log(arr3);
+//antra uzduotis
+var masyvas1=[1,2,3];
+var apverstas=masyvas1.reverse();
+console.log(apverstas);
+// trecia uzduotis
+
+var mas=[1,2,3];
+mas.push(4,5,6);
+mas.unshift(-3,-2,-1);
+console.log(mas);
+
+// ketvirta uzduotis
+
+var m=["html","css", "js"];
+console.log(m.slice(0,2));
+
+//penkta uzduotis
+
+var kazkas=[3,4,1,2,7];
+console.log(kazkas.)
 
