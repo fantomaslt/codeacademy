6e4fa9b5dc2711bc75c4558f27e2b411199135df
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-05 17:19:19 +0200

    aaaaa

--- Classworks/02-27/app.js
+++ Classworks/02-27/app.js
@@ -183,4 +183,66 @@ alert("jusu sutapimas yra " + loveScore + " % ");
 
 
 
+//10 uzduotis
+var dvimatis={ lt:["pirmadienis", "antradienis","treciadienis", "ketvirtadienis", "penktadienis", "sestadienis", "sekmadienis"],
+    en:["monday", "thuesday", "wensday", "thursday", "friday", "saturday","sunday"]};
+console.log(dvimatis.lt[0], " ", dvimatis.en[2]);
 
+// 11 uzduotis
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//     Duotas masyvas 3, 4, 1, 2, 7. Surūšiuokite jį.
+//     Duotas masyvas 1, 2, 3, 4, 5. Konvertuokite masyvą į 1, 2, 3.
+// Duotas masyvas [1, 2, 3, 4, 5]. Konvertuokite masyvą į [1, 4, 5].
+//     For ir While
+// Atspausdinkite skaičių stulpelį nuo 1 iki 100.
+// Atspausdinkite skaičių stulpelį nuo 11 iki 33.
+// Atspausdinkite stulpelį su lyginiais skaičiais nuo 0 iki 100.
+// Naudodami ciklą parodykite sumą nuo 1 iki 100.
+// For ir masyvai
+// Duotas masyvas su elementais [1, 2, 3, 4, 5]. Parodykite visus masyvo elementus ekrane.
+//     Duotas masyvas su elementais [1, 2, 3, 4, 5]. Parodykite šio masyvo elementų sumą.
+//     for-in
+// Duotas objektas green: žalia, red: raudona, blue: mėlyna. Parodykite šio objekto raktus ir elementus.
+//     Duotas objektas su raktais Mantas, Paulius, Mindaugas su reikšmėm 200, 300, 300. Parodykite eilutes tokiu formatu: Mantas - 200 EU atlyginimas.
+//     Duotas masyvas su elementais 2, 5, 9, 15, 0, 4. Naudodami for ir if parodykite masyvo elementus kurie yra daugiau nei 3, bet mažiau nei 10
+// Duotas masyvas su skaičiais. Skaičiai gali būti teigiami ir neigiami. Raskite teigiamų masyvo skaičių sumą.
+//     Duotas masyvas su elementais [1, 2, 3, 4, 5]. Parodykite visus šio masyvo elementus pasinaudoję ciklais for, while
+//     Duotas masyvas su elementais [2, 3, 4, 5]. Parodykite šio masyvo elementų produktą (daugyba), naudokite for ciklą.
+//     Duotas objektas su raktais Vilnius, Riga, Talinas ir reikšmėm Lietuva, Latvija, Estija. Parodykite eilutes tokiu formatu: Vilnius yra Lietuva naudodami for-in ciklą;
+
+
+// //pirma uzduotis
+
+var arr1=[1,2,3];
+var arr2=[4,5,6];
+var arr3 = arr1.concat(arr2);
+console.log(arr3);
+//antra uzduotis
+var masyvas1=[1,2,3];
+var apverstas=masyvas1.reverse();
+console.log(apverstas);
+// trecia uzduotis
+
+var mas=[1,2,3];
+mas.push(4,5,6);
+mas.unshift(-3,-2,-1);
+console.log(mas);
+
+// ketvirta uzduotis
+
+var m=["html","css", "js"];
+console.log(m.slice(0,2));
+
+//penkta uzduotis
+
+var kazkas=[3,4,1,2,7];
+console.log(kazkas.);
+
+asd g g gds g
