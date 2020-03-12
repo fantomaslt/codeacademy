2e36812920da0091e4e6d3bf54e60ffa5bc32254
fantomaslt <buta.karolis@gmail.com>
2020-03-12 19:18:01 +0200

    b

--- Homeworks/03-09/homework.js
+++ Homeworks/03-09/homework.js
@@ -1,34 +1,43 @@
 // Parašykite funkciją, kuri grąžina skaičiaus kvadratą. Skaičius perduodamas kaip parametras.
 
 function square(number) {
-    return number*number;
+    return number * number;
 }
+
 console.log(square(5));
 
 
 // Parašykite funkciją, kuri grąžina dviejų skaičių sumą. Skaičiai perduodami funkcijos parametrus.
 
-function sum(a,b) {
-    return a+b;
+function sum(a, b) {
+    return a + b;
 }
-console.log(sum(2,3));
+
+console.log(sum(2, 3));
 
 // Parašykite funkciją, kuri iš antro parametro atima pirmą ir padalija iš trečio.
 
-function result(c,d,e) {
-    return  (d-c)/e;
-
+function result(c, d, e) {
+    return (d - c) / e;
 }
-console.log(result(2,4, 2 ));
+
+console.log(result(2, 4, 2));
 
 
 // Parašykite funkciją, kuri priima kaip parametrą skaičių nuo 1 iki 7, o grąžina savaitės dieną lietuvių kalba.
-let days=['pirmadienis','antradienis','treciadienis','ketvirtadienis','penktadienis','sestadienis','sekmadienis'];
+let days = [
+    'pirmadienis',
+    'antradienis',
+    'treciadienis',
+    'ketvirtadienis',
+    'penktadienis',
+    'sestadienis',
+    'sekmadienis'
+];
 function dayOfTheWeek(number) {
     if (number < 1 || number > 7) {
         return 'Tokios savaitės dienos nėra'
     }
-
     return days[number-1];
 }
 console.log(dayOfTheWeek(2));
