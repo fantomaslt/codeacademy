3ecc3c98c8bed7289012b43a3cc43ba9157840b1
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-15 21:00:45 +0200

    namu darbai

--- Homeworks/03-12/js.js
+++ Homeworks/03-12/js.js
@@ -3,9 +3,26 @@ function myFunction() {
 }
 
 function changeToUpperCase() {
-    document.getElementById("Fill_me").value = document.getElementById("Fill_me").value.toUpperCase().replace(" ", "")
+    document.getElementById("Fill_me").value = document.getElementById("Fill_me").value.toUpperCase().replace(" ", " ")
 }
 
 function changeToLowerCase() {
-    document.getElementById("Fill_me").value = document.getElementById("Fill_me").value.toLowerCase().replace(" ", "")
+    document.getElementById("Fill_me").value = document.getElementById("Fill_me").value.toLowerCase().replace(" ", " ")
+}
+
+
+
+
+
+
+
+
+
+
+function disable() {
+    document.getElementById("en/dsbl").disabled = true;
+}
+
+function enable() {
+    document.getElementById("en/dsbl").disabled = false;
 }
