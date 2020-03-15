12b408f07113b3b170e2535c1d212aaa4702803a
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-15 22:00:46 +0200

    namu darbai

--- Homeworks/03-12/js.js
+++ Homeworks/03-12/js.js
@@ -18,11 +18,28 @@ function changeToLowerCase() {
 
 
 
-
+// ijungia isjungia inputa
 function disable() {
     document.getElementById("en/dsbl").disabled = true;
 }
 
 function enable() {
     document.getElementById("en/dsbl").disabled = false;
+}
+
+// uzvedus pakeicia paveiksliuka
+
+
+
+
+
+document.getElementById("imgg").onmouseover = function() {mouseOver()};
+document.getElementById("imgg").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById("imgg").src = "hackanm.gif";
+}
+
+function mouseOut() {
+    document.getElementById("imgg").src = "https://i.imgur.com/0DElr0H.jpg";
 }
