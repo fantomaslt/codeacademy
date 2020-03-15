991da49368a14f39e45a4b7879925e3a44481cbd
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-15 20:37:06 +0200

    namu darbai

--- Homeworks/03-12/js.js
+++ Homeworks/03-12/js.js
@@ -1,3 +1,11 @@
 function myFunction() {
     alert("Hello World");
 }
+
+function changeToUpperCase() {
+    document.getElementById("Fill_me").value = document.getElementById("Fill_me").value.toUpperCase().replace(" ", "")
+}
+
+function changeToLowerCase() {
+    document.getElementById("Fill_me").value = document.getElementById("Fill_me").value.toLowerCase().replace(" ", "")
+}
