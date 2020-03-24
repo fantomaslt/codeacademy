b117b9d7582ef05d6be6dc23fdbd8994a615e558
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-24 15:00:32 +0200

    namu darbai pataisymai

---
+++ Homeworks/03-16/js.js
@@ -0,0 +1,39 @@
+
+let AllRadioBtn = document.querySelectorAll(".w").length;
+ for (var i = 0; i<AllRadioBtn; i++) {
+
+    document.querySelectorAll(".btn-radio")[i].addEventListener("click", function() {
+
+        // alert("aaaa");
+
+let shapes =
+
+
+
+    switch (shapes) {
+        case "a":
+            document.querySelector(".squere") = document.getElementById("w").style.display = "block";
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        default: style="display:none;"
+
+    }
+
+
+
+
+ //
+ //        if (document.querySelectorAll(". squere")) {
+ //            document.getElementById("w").style.display = "block";
+ //        } else if (document.querySelectorAll(".w")) {
+ //            document.getElementById("c").style.display = "block";
+ //        }
+ //
+ //
+ //    })
+ // }
---
+++ Homeworks/03-23/index.php
@@ -0,0 +1,4 @@
+<?php
+/*php followed by tab*/
+asd ad
+ ?>
