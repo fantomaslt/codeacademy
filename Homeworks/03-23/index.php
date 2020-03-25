6aef2ab26886d4fceff0e46e6cb4f2207412a954
fantomaslt <61119532+fantomaslt@users.noreply.github.com>
2020-03-25 11:44:26 +0200

    namu darbai pataisymai

--- Homeworks/03-23/index.php
+++ Homeworks/03-23/index.php
@@ -1,4 +1,16 @@
+
 <?php
-/*php followed by tab*/
-asd ad
- ?>
+// Create a stream
+$opts = array(
+    'http'=>array(
+        'method'=>"GET",
+        'header'=>"Accept-language: en\r\n" .
+            "Cookie: foo=bar\r\n"
+    )
+);
+
+$context = stream_context_create($opts);
+
+// Open the file using the HTTP headers set above
+$file = file_get_contents('http://www.example.com/', false, $context);
+?>
